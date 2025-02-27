9d7fbb65cad77cf1088243405c53621d1b729650
xiepg <xiepg@meixing.com>
2025-02-27 10:47:18 +0800

    系统的实例

--- examples/system/system.rs
+++ examples/system/system.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 use bevy::window::WindowResolution;
 use bevy::DefaultPlugins;
 use std::slice::Windows;
+use bevy::input::common_conditions::input_just_pressed;
 
 fn main() {
     #[cfg(target_os = "windows")]
@@ -17,18 +18,42 @@ fn main() {
             }),
             ..default()
         }))
-        .add_systems(Startup, || {
-            info!("Startup");
-        })
-        .add_systems(Update, || {
-            info!("Updating");
-        })
-        .add_systems(FixedUpdate, || {
-            info!("FixedUpdate");
-        })
+        // .add_systems(Startup, || {
+        //     info!("Startup");
+        // })
+        // .add_systems(Update, || {
+        //     info!("Updating");
+        // })
+        // .add_systems(FixedUpdate, || {
+        //     info!("FixedUpdate");
+        // })
+        // .add_systems(Update, system_a.after(system_b))
+        // .add_systems(Update, system_a.before(system_b))
+        // .add_systems(Update, (system_c, system_b, system_a).chain())
+        // .add_systems(Update, system_a.run_if(input_just_pressed(KeyCode::Space)))
+        .add_systems(Update, system_c.run_if(time_passed(2f32)))
         .run();
 }
 
 fn update_system() {
     info!("Updating system");
+}
+
+fn system_a() {
+    info!("system_a")
+}
+fn system_b() {
+    info!("system_b")
+}
+fn system_c() {
+    info!("system_c")
+}
+
+fn time_passed(t: f32) -> impl FnMut(Local<f32>, Res<Time>) -> bool {
+    move |mut timer: Local<f32>, time: Res<Time>| {
+        // Tick the timer
+        *timer += time.delta_secs();
+        // Return true if the timer has passed the time
+        *timer >= t
+    }
 }
